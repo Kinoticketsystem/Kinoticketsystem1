3ff73557bc84d9903c91a240ba1653d11ed4fc93
Thiemo2 <Thiemo2@users.noreply.github.com>
2019-09-12 17:15:09 +0200

    Testinterface und erste Form angelegt

---
+++ Kino/Kinoticketsystem/Form1.vb
@@ -0,0 +1,25 @@
+﻿Imports Kinoticketsystem
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+End Class
+
+Public Interface Testinterface
+    Sub Testprozedur(ByRef a As Integer)
+    Function TestFunktion(ByVal b As Integer)
+End Interface
+
+Public Class Testklasse
+    Implements Testinterface
+
+    Public Sub Testprozedur(ByRef a As Integer) Implements Testinterface.Testprozedur
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Function TestFunktion(b As Integer) As Object Implements Testinterface.TestFunktion
+        Throw New NotImplementedException()
+    End Function
+End Class
