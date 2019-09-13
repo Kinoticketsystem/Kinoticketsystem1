e8055313a0057799cfd686a704917382c9f8d064
Thiemo2 <Thiemo2@users.noreply.github.com>
2019-09-13 17:26:59 +0200

    Klasse Tagesplan und Interface Tagesplan implementiert

--- Kino/Kinoticketsystem/Form1.vb
+++ Kino/Kinoticketsystem/Form1.vb
@@ -10,32 +10,37 @@ Public Class FTagesplan
     End Sub
 End Class
 
-'Ein Tagesplan mit
-Public Interface Tagesplan
-    Sub TagesplanErstellen(ByVal Vorstellung1 As Vorstellung, ByVal Vorstellung2 As Vorstellung, ByVal Vorstellung3 As Vorstellung, ByVal Vorstellung4 As Vorstellung)
-
+'Ein Tagesplan, der speichert wann welcher Film gezeigt wird
+'Beim Erstellen müssen die Filme in der richtigen Reihenfolge übergeben werden
+Public Interface ITagesplan
+    Sub TagesplanErstellen(ByVal Vorstellung1 As Vorstellung, ByVal Vorstellung2 As Vorstellung, ByVal Vorstellung3 As Vorstellung, ByVal Vorstellung4 As Vorstellung, ByVal Vorstellung5 As Vorstellung)
+    'Es muss die Filmnummer übergeben werden (später vielleicht mal die Vorstellung mit der getauscht werden soll oder die Uhrzeit?)
+    Sub FilmÄndern(ByVal Vorstellung As Vorstellung, ByVal Filmnummer As Integer)
+    'wird ans Ende hinzugefügt
+    Sub FilmHinzufügen(ByVal Vorstellung As Vorstellung)
+    Sub FilmEntfernen(ByVal vorstellung As Vorstellung, ByVal Position As Integer)
 End Interface
 
-Public Interface Vorstellung
+Public Interface IVorstellung
 
 End Interface
-
+'Gabor
 Public Class Kinosaal
 
 End Class
-Public Interface Testinterface
+Public Interface ITestinterface
     Sub Testprozedur(ByRef a As Integer)
     Function TestFunktion(ByVal b As Integer)
 End Interface
 
 Public Class Testklasse
-    Implements Testinterface
+    Implements ITestinterface
 
-    Public Sub Testprozedur(ByRef a As Integer) Implements Testinterface.Testprozedur
+    Public Sub Testprozedur(ByRef a As Integer) Implements ITestinterface.Testprozedur
         Throw New NotImplementedException()
     End Sub
 
-    Public Function TestFunktion(b As Integer) As Object Implements Testinterface.TestFunktion
+    Public Function TestFunktion(b As Integer) As Object Implements ITestinterface.TestFunktion
         Throw New NotImplementedException()
     End Function
 End Class
---
+++ Kino/Kinoticketsystem/Tagesplan.vb
@@ -0,0 +1,42 @@
+﻿Imports Kinoticketsystem
+
+Public Class Tagesplan
+    Implements ITagesplan
+    Private _Vorstellungen(MaximaleAnzahlFilmeProTag) As Vorstellung
+    Private MaximaleAnzahlFilmeProTag As Integer = 10
+    Private AnzahlFilmeProTag As Integer
+
+    Public Sub New()
+        _Vorstellungen = New Vorstellung() {}
+    End Sub
+    Public Sub TagesplanErstellen(Vorstellung1 As Vorstellung, Vorstellung2 As Vorstellung, Vorstellung3 As Vorstellung, Vorstellung4 As Vorstellung, Vorstellung5 As Vorstellung) Implements ITagesplan.TagesplanErstellen
+        _Vorstellungen(0) = Vorstellung1
+        _Vorstellungen(2) = Vorstellung2
+        _Vorstellungen(3) = Vorstellung3
+        _Vorstellungen(4) = Vorstellung4
+        _Vorstellungen(5) = Vorstellung5
+        AnzahlFilmeProTag = 5
+        'For i As Integer = 0 To MaximaleAnzahlFilmeProTag - 1
+
+        'Next
+    End Sub
+    'Filmnummer zwischen [1 und MaximaleAnzahlFilmeProTag]
+    Public Sub FilmÄndern(Vorstellung As Vorstellung, Filmnummer As Integer) Implements ITagesplan.FilmÄndern
+        _Vorstellungen(Filmnummer + 1) = Vorstellung
+    End Sub
+
+    Public Sub FilmHinzufügen(Vorstellung As Vorstellung) Implements ITagesplan.FilmHinzufügen
+        _Vorstellungen(AnzahlFilmeProTag) = Vorstellung
+        AnzahlFilmeProTag = AnzahlFilmeProTag + 1
+    End Sub
+
+    Public Sub FilmEntfernen(vorstellung As Vorstellung, Position As Integer) Implements ITagesplan.FilmEntfernen
+        _Vorstellungen(AnzahlFilmeProTag) = Nothing
+        AnzahlFilmeProTag = AnzahlFilmeProTag - 1
+    End Sub
+End Class
+
+Public Class Vorstellung
+    Implements IVorstellung
+
+End Class
