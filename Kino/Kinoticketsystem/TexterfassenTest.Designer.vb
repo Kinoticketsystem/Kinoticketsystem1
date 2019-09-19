1929c3d7b72f7b28348a97f4730fc5a777db5337
Erik Fischer <Fischer-Erik@hotmail.com>
2019-09-19 08:39:11 +0200

    Neue Form zum Einlesen von Textdateien

---
+++ Kino/Kinoticketsystem/TexterfassenTest.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TexterfassenTest
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstTextausgabe = New System.Windows.Forms.ListBox()
+        Me.cmdTexterfassen = New System.Windows.Forms.Button()
+        Me.txtInput = New System.Windows.Forms.TextBox()
+        Me.SuspendLayout()
+        '
+        'lstTextausgabe
+        '
+        Me.lstTextausgabe.FormattingEnabled = True
+        Me.lstTextausgabe.Location = New System.Drawing.Point(36, 12)
+        Me.lstTextausgabe.Name = "lstTextausgabe"
+        Me.lstTextausgabe.Size = New System.Drawing.Size(589, 355)
+        Me.lstTextausgabe.TabIndex = 0
+        '
+        'cmdTexterfassen
+        '
+        Me.cmdTexterfassen.Location = New System.Drawing.Point(689, 89)
+        Me.cmdTexterfassen.Name = "cmdTexterfassen"
+        Me.cmdTexterfassen.Size = New System.Drawing.Size(175, 50)
+        Me.cmdTexterfassen.TabIndex = 1
+        Me.cmdTexterfassen.Text = "TextErfassen"
+        Me.cmdTexterfassen.UseVisualStyleBackColor = True
+        '
+        'txtInput
+        '
+        Me.txtInput.Location = New System.Drawing.Point(84, 424)
+        Me.txtInput.Name = "txtInput"
+        Me.txtInput.Size = New System.Drawing.Size(168, 20)
+        Me.txtInput.TabIndex = 2
+        '
+        'TexterfassenTest
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1166, 474)
+        Me.Controls.Add(Me.txtInput)
+        Me.Controls.Add(Me.cmdTexterfassen)
+        Me.Controls.Add(Me.lstTextausgabe)
+        Me.Name = "TexterfassenTest"
+        Me.Text = "TexterfassenTest"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lstTextausgabe As ListBox
+    Friend WithEvents cmdTexterfassen As Button
+    Friend WithEvents txtInput As TextBox
+End Class
---
+++ Kino/Kinoticketsystem/TexterfassenTest.vb
@@ -0,0 +1,23 @@
+﻿Public Class TexterfassenTest
+    Public Sub texterfassen()
+
+
+        FileOpen(1, "Speichertext.txt", OpenMode.Input)
+        While Not EOF(1)
+            lstTextausgabe.Items.Add(LineInput(1))
+            ' in der Listbox wird der ganze Text aus der Datei angezeigt (Zeile für Zeile ausgelesen)
+            Console.WriteLine()
+        End While
+        FileClose(1)
+    End Sub
+
+    Public Sub texteinfügen()
+        Dim neuertext As String = txtInput.Text
+        'textbox ist die, in der der einzulesende Text geschrieben wird, 
+        'dieser wird dann in der nächsten Zeile der Datei angefügt 
+
+        FileOpen(1, "test.txt", OpenMode.Append)
+        PrintLine(1, neuertext)
+        FileClose(1)
+    End Sub
+End Class
