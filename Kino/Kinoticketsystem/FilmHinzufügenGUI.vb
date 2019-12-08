cd1ea421cd762413913d78e1220bae74153de8e7
Gabor Fogarasi <gabor@matrix-sicherheitstechnik.de>
2019-12-08 14:53:23 +0100

    Filmhinzu

--- Kino/Kinoticketsystem/FilmHinzufügenGUI.vb
+++ Kino/Kinoticketsystem/FilmHinzufügenGUI.vb
@@ -1,9 +1,12 @@
 ﻿Public Class FilmHinzufügenGUI
-    Private Filmname As String = txtFilmtitel.Text
-    Private Filmlänge As Integer = txtFilmlänge.Text
-    Private Altersfreigabe As Byte = txtAltersfreigabe.Text
+    Private Filmname As String
+    Private Filmlänge As Integer
+    Private Altersfreigabe As Byte
     Private _3d As Boolean
     Private Sub cmdFilmHinzufügen_Click(sender As Object, e As EventArgs) Handles cmdFilmHinzufügen.Click
+        Filmname = txtFilmtitel.Text
+        Filmlänge = txtFilmlänge.Text
+        Altersfreigabe = txtAltersfreigabe.Text
         If CheckBox1.Checked Then
             _3d = True
         Else
@@ -13,5 +16,9 @@
         Dim film As Film = New Film(Filmname, Filmlänge, Altersfreigabe, _3d)
         KinoGUI.DASKINO.FilmHinzufügen(film)
         'Es muss noch in der listbox alle filntitel ausgegebn werden um zu überprüfen,m ob der neue film übergebn wurde ist bei kinogui geschrieben
+        Dim a As ArrayList = KinoGUI.DASKINO.getFilmtitel()
+        For i = 0 To KinoGUI.DASKINO.getFilmtitel.Count - 1 '-1 richtig?
+            lstAlleFilme.Items.Add(a(i))
+        Next
     End Sub
 End Class
--- Kino/Kinoticketsystem/Kunden.vb
+++ Kino/Kinoticketsystem/Kunden.vb
@@ -1,10 +1,16 @@
 ﻿Public Class KundenGUI
-    Private Sub KundenGUI_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+    Private Sub KundenGUI_Load(sender As Object, e As EventArgs) Handles Me.Load
+        Dim name As String = txtName.Text
+        Dim a As Kunde = New Kunde(name)
+        Dim b As ArrayList
     End Sub
     Public Sub KundenÜbergeben(a As ArrayList)
+
         For i = 0 To a.Count
             lstSammlung.Items.Add(a)
         Next
     End Sub
+
+
 End Class
