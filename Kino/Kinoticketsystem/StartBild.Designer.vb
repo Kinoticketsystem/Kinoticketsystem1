98c320eeffd06b44ae859e3c86002fa2950b979d
Gabor Fogarasi <gabor@matrix-sicherheitstechnik.de>
2019-12-11 21:54:40 +0100

    STartbildschirm; Startformulat ändern unter MyProject falss schnellerer Start erwünscht

--- Kino/Kinoticketsystem/StartBild.Designer.vb
+++ Kino/Kinoticketsystem/StartBild.Designer.vb
@@ -25,6 +25,7 @@ Partial Class StartBild
         Me.components = New System.ComponentModel.Container()
         Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
@@ -35,6 +36,8 @@ Partial Class StartBild
         '
         'PictureBox1
         '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Silver
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
         Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Fill
         Me.PictureBox1.Image = Global.Kinoticketsystem.My.Resources.Resources.Kinologo
         Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
@@ -44,20 +47,36 @@ Partial Class StartBild
         Me.PictureBox1.TabIndex = 1
         Me.PictureBox1.TabStop = False
         '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Label1.Location = New System.Drawing.Point(1888, 1494)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(852, 55)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "By: Erik,Thiemo, Jesse, Malte, Gabor"
+        '
         'StartBild
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.White
         Me.ClientSize = New System.Drawing.Size(3026, 1748)
+        Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.PictureBox1)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "StartBild"
         Me.Text = "Kinoticketsystem"
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
     Friend WithEvents Timer1 As Timer
     Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Label1 As Label
 End Class
--- Kino/Kinoticketsystem/StartBild.vb
+++ Kino/Kinoticketsystem/StartBild.vb
@@ -1,17 +1,26 @@
 ﻿Public Class StartBild
 
-    Dim i As Integer = 0
+    Private i As Integer = 0
 
-    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
-        i = i + 1
-        If i > 3 Then
-            Timer1.Stop()
-            Me.Close()
-            KinoGUI.Show()
-        End If
+    Private Sub StartBild_Activated(sender As Object, e As EventArgs) Handles Me.Activated
+        KinoGUI.Activate()
+        Cursor.Hide()
+        Me.CenterToScreen()
+        Me.WindowState = FormWindowState.Maximized
+
     End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+
+        i = i + 1
+        If i > 6 Then
+            Timer1.Stop()
+            KinoGUI.Show()
+            Me.Close()
+            Cursor.Show()
+        End If
 
 
+    End Sub
 
 
 End Class
