b82afe52fe70be7ed8642190114562b94619ed03
Gabor Fogarasi <gabor@matrix-sicherheitstechnik.de>
2019-12-16 15:52:27 +0100

    neues startbild

--- Kino/Kinoticketsystem/My Project/Resources.Designer.vb
+++ Kino/Kinoticketsystem/My Project/Resources.Designer.vb
@@ -63,6 +63,16 @@ Namespace My.Resources
         '''<summary>
         '''  Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property _720px() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("_720px", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property Alita_Battle_Angel() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("Alita_Battle_Angel", resourceCulture)
