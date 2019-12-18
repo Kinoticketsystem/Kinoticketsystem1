83cb8f0cf2c770e22e408c139fc9cc223888b523
Erik Fischer <Fischer-Erik@hotmail.com>
2019-12-18 19:32:53 +0100

    zuziz

--- Kino/Kinoticketsystem/KinosaalHinzufügenGUI.vb
+++ Kino/Kinoticketsystem/KinosaalHinzufügenGUI.vb
@@ -3,22 +3,18 @@
         If IsNumeric(txtAnzahlReihe.Text) And IsNumeric(txtSitzeproReihe.Text) = True Then
             Dim plätze As Integer
             Dim reihen As Integer = txtAnzahlReihe.Text
+            reihen = Math.Abs(reihen)
             Dim sitze As Integer = txtSitzeproReihe.Text
-            If txtAnzahlReihe.Text > 200 Then
-                MsgBox("Saal Zugroß")
+            sitze = Math.Abs(sitze)
+            If reihen > 8 Then
+                MsgBox("Maximal 8 Reihen!")
+            ElseIf sitze > 15 Then
+                MsgBox("Maximal 15 Sitze pro Reihe!")
             Else
                 plätze = reihen * sitze
                 KinoGUI.DASKINO.KinosaalAmEndeHinzufügen(New Kinosaal(plätze, reihen, sitze))
                 MsgBox("Der neue Saal wurde hinzugefügt!")
             End If
-            If txtSitzeproReihe.Text > 200 Then
-                MsgBox("Saal Zugroß")
-            Else
-                plätze = reihen * sitze
-                KinoGUI.DASKINO.KinosaalAmEndeHinzufügen(New Kinosaal(plätze, reihen, sitze))
-                MsgBox("Der neue Saal wurde hinzugefügt!")
-            End If
-
         Else
             MsgBox("Bitte geben sie Zahlenwerte in die Textfelder ein, um einen neuen Kinosaal zu erstellen!")
         End If
