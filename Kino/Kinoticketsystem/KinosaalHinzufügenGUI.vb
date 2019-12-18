00a780309a32ef76774021a1af1723c97526ec2e
Malte Fischer <maltefischer2011@outlook.de>
2019-12-18 19:19:06 +0100

    Saal hinzufügen geändert

--- Kino/Kinoticketsystem/KinosaalHinzufügenGUI.vb
+++ Kino/Kinoticketsystem/KinosaalHinzufügenGUI.vb
@@ -4,9 +4,21 @@
             Dim plätze As Integer
             Dim reihen As Integer = txtAnzahlReihe.Text
             Dim sitze As Integer = txtSitzeproReihe.Text
-            plätze = reihen * sitze
-            KinoGUI.DASKINO.KinosaalAmEndeHinzufügen(New Kinosaal(plätze, reihen, sitze))
-            MsgBox("Der neue Saal wurde hinzugefügt!")
+            If txtAnzahlReihe.Text > 200 Then
+                MsgBox("Saal Zugroß")
+            Else
+                plätze = reihen * sitze
+                KinoGUI.DASKINO.KinosaalAmEndeHinzufügen(New Kinosaal(plätze, reihen, sitze))
+                MsgBox("Der neue Saal wurde hinzugefügt!")
+            End If
+            If txtSitzeproReihe.Text > 200 Then
+                MsgBox("Saal Zugroß")
+            Else
+                plätze = reihen * sitze
+                KinoGUI.DASKINO.KinosaalAmEndeHinzufügen(New Kinosaal(plätze, reihen, sitze))
+                MsgBox("Der neue Saal wurde hinzugefügt!")
+            End If
+
         Else
             MsgBox("Bitte geben sie Zahlenwerte in die Textfelder ein, um einen neuen Kinosaal zu erstellen!")
         End If
